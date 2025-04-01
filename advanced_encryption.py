e53c2cebd39b706e48f8aa94101b3eb1818b263f
mefathim <v4105677@gmail.com>
2025-04-01 11:19:52 +0300

    yehuda

--- advanced_encryption.py
+++ advanced_encryption.py
@@ -11,28 +11,25 @@ def get_sorted_indexes(key_word, encrypt=True):
     return reverse_positions
 
 def transposition_cipher(path, key_word, encrypt=True):
-    try:
-        text = read_file(path)
-        result = ""
-        if encrypt:
-            text += " " * (-len(text) % len(key_word))
-            keyword_indexed = get_sorted_indexes(key_word)
-            skips = len(key_word)
-            for i in keyword_indexed:
-                for j in range(i, len(text), skips):
-                    result += text[j]
-            write_result_file(path, result, encrypt)
-        else:
-            keyword_indexed = get_sorted_indexes(key_word, False)
-            skips = len(text) // len(key_word)
-            for i in range(skips):
-                for j in keyword_indexed:
-                    result += text[j * skips + i]
-            write_result_file(path, result, encrypt)
-
-
-    except FileNotFoundError:
-        print(f"Error: File '{path}' not found.")
+    text = read_file(path)
+    if len(key_word) < 2:
+        return print("For good encryption performance is required a key-word that is longer than 1 letter")
+    result = ""
+    if encrypt:
+        text += " " * (-len(text) % len(key_word))
+        keyword_indexed = get_sorted_indexes(key_word)
+        skips = len(key_word)
+        for i in keyword_indexed:
+            for j in range(i, len(text), skips):
+                result += text[j]
+        write_result_file(path, result, encrypt)
+    else:
+        keyword_indexed = get_sorted_indexes(key_word, False)
+        skips = len(text) // len(key_word)
+        for i in range(skips):
+            for j in keyword_indexed:
+                result += text[j * skips + i]
+        write_result_file(path, result, encrypt)
 
 def rsa_cipher(path, key_word, encrypt=True):
     pass
